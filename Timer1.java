8423f5a74c1bf2d258a55765c1adcbf9de6d5d65
Teijmen <teijmenploeg@gmail.com>
2020-01-20 15:22:11 +0100

    Timer implemented v1.0

--- Timer1.java
+++ Timer1.java
@@ -9,14 +9,31 @@ import java.util.TimerTask;
 public class Timer1
 {
     private boolean clockRunning = false;
-    int secondPassed = 900000; //15 min
-    
+    int secondPassed = 900; //15 min
     Timer timer = new Timer();
     TimerTask task = new TimerTask(){
         public void run(){
            if(clockRunning == true){
                 secondPassed--;
-                System.out.println(secondPassed);
+                if(secondPassed == 899){
+                    System.out.println("You have 15 minutes left to escape!");
+                }
+                if(secondPassed == 600){
+                    System.out.println("You have 10 minutes left to escape!");
+                }
+                if(secondPassed == 300){
+                    System.out.println("You have 5 minutes left to escape!");
+                }
+                if(secondPassed == 60){
+                    System.out.println("You have 1 minute left to escape!");
+                }
+                if(secondPassed == 30){
+                    System.out.println("You have 30 seconds left to escape!");
+                }
+                if(secondPassed == 1){
+                    System.out.println("Time is up! You got caught!");
+                    clockRunning = false;
+                }
                 //int minutes = (secondPassed / 1000) / 60;
                 //int seconds = (secondPassed / 1000) % 60;
            }
@@ -25,8 +42,8 @@ public class Timer1
     };
     public void getTime(){
         int timer = secondPassed;
-        int minutes = (secondPassed / 1000) / 60;
-        int seconds = (secondPassed / 1000) % 60;
+        int minutes = secondPassed / 60;
+        int seconds = secondPassed % 60;
         System.out.println(minutes + ":" + seconds);
     }
     
